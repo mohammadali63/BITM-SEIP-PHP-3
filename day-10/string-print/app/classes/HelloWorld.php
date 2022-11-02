868cccb31214ecf74c5ec4f766ea3abaa68e17b3
mohammadali63 <taheralinur@gmail.com>
2022-11-02 19:16:03 +0600

    First Name & Last Name print

--- day-10/string-print/app/classes/HelloWorld.php
+++ day-10/string-print/app/classes/HelloWorld.php
@@ -4,5 +4,14 @@ namespace App\classes;
 
 class HelloWorld
 {
+    public $name;
+    public $email;
+    public $mobile;
+    public function index()
+    {
+        $firstName="Mohammad";
+        $lastName="Ali";
+        echo "Full Name is ".$firstName.' '.$lastName;
+    }
 
 }
