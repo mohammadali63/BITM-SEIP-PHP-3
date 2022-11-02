4399b6a4d4ef7c195cc44a7ee9e41574c3a2ccef
mohammadali63 <taheralinur@gmail.com>
2022-11-02 19:45:12 +0600

    Arithmatic Binary Oparetor coding

--- day-10/arithmatic-oparetor/app/classes/ArithmaticOparetor.php
+++ day-10/arithmatic-oparetor/app/classes/ArithmaticOparetor.php
@@ -4,5 +4,24 @@ namespace App\classes;
 
 class ArithmaticOparetor
 {
+    public $x;
+    public $y;
+
+    public function index()
+    {
+        $this->x=10;
+        $this->y=20;
+
+        echo $this->x + $this->y;
+        echo'<br/>';
+        echo $this->x - $this->y;
+        echo'<br/>';
+        echo $this->x * $this->y;
+        echo'<br/>';
+        echo $this->x / $this->y;
+        echo'<br/>';
+        echo $this->x % $this->y;
+        echo'<br/>';
+    }
 
 }
