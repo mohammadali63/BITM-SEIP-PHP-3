54caec532f8f0760f32dddae33d3e0568afac351
mohammadali63 <taheralinur@gmail.com>
2022-11-03 21:05:28 +0600

    foreach loop run

---
+++ day-10/foreach-loop/app/classes/ForeachLoop.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\classes;
+
+class ForeachLoop
+{
+    public $x,$data;
+    public function __construct()
+    {
+        $this->x="hello";
+    }
+    public function index()
+    {
+        $this->data=[ 10, 20, 30, "BITM", 100.450, true];
+        foreach ($this->data as $item)
+        {
+            echo $item;
+        }
+    }
+
+}
---
+++ day-10/foreach-loop/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once "vendor/autoload.php";
+use App\classes\ForeachLoop;
+
+$hello=new ForeachLoop();
+$hello->index();
