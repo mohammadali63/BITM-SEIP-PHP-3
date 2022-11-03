b560e5f3bde153b59ebe0316bb11e0d2cca5728e
mohammadali63 <taheralinur@gmail.com>
2022-11-03 20:20:18 +0600

    array position 3 print

--- day-10/array/app/classes/ArrayPhp.php
+++ day-10/array/app/classes/ArrayPhp.php
@@ -4,5 +4,15 @@ namespace App\classes;
 
 class ArrayPhp
 {
+    public $x,$data;
+    public function __construct()
+    {
+        $this->x="hello";
+    }
+    public function index()
+    {
+        $this->data=[ 10, 20, 30, "BITM", 100.450, true];
+        echo $this->data[9];
+    }
 
 }
