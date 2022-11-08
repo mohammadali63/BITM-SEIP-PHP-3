62470a51c8d7f390c2a784fb0d090f2b1b641b82
mohammadali63 <taheralinur@gmail.com>
2022-11-08 12:04:09 +0600

    Day-11 multiply array php project

--- day-11/array/app/classes/ArrayPhp.php
+++ day-11/array/app/classes/ArrayPhp.php
@@ -4,5 +4,84 @@ namespace App\classes;
 
 class ArrayPhp
 {
+    public $student = [];
 
+    public function index()
+    {
+        $this->student = [
+            0 => [
+                'name' => 'jahid',
+                'email' => 'jahi@gmail.com',
+                [
+                    'mobile1' => '0978656',
+                    'mobile2' => '97856754'
+                ],
+            ],
+            1 => [
+                'name' => 'ali',
+                'email' => 'ali@gmail.com',
+                [
+                    'mobile1' => '0978656',
+                    'mobile2' => '97856754'
+                ],
+            ],
+
+            2 => 'BITM',
+
+            3 => [
+                'name' => 'jihad',
+                'email' => 'jihad@gmail.com',
+                [
+                    'mobile1' => '0978656',
+                    'mobile2' => '97856754'
+                ],
+            ],
+        ];
+        foreach ($this->student as $item)
+        {
+            if (is_array($item))
+            {
+                foreach ($item as $value)
+                {
+                    if (is_array($value))
+                    {
+                      foreach ($value as $v_value)
+                      {
+                          echo $v_value;
+                      }
+
+                    }
+                    else
+                    {
+                        echo $value.'';
+                    }
+
+                }
+            }
+            else
+            {
+                echo $item;
+            }
+            echo '<br/>';
+        }
+    }
 }
+
+
+
+
+
+//        foreach ($this->student as $item)
+//        {
+//            if (is_array($item))
+//            {
+//                foreach ($item as $value)
+//                {
+//                    echo $value.'';
+//                }
+//            }
+//            else{
+//                echo $item;
+//            }
+//            echo '<br/>';
+//        }
